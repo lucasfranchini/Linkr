5f0ff8389e3ad22c7f6a319f8df7a290e7bd9b98
lucasfranchini <lucasfranchini.a@gmail.com>
2021-05-24 19:36:37 -0300

    adicionando reset.css e configurando o index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
+import "./Reset.css";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
